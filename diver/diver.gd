cfd99762c75558a3f64990bc5aa22ed856dc3237
Bak <bakkdorl@gmail.com>
2026-03-24 19:03:18 -0300

    sprites novos

--- diver/diver.gd
+++ diver/diver.gd
@@ -4,8 +4,15 @@ extends Area2D
 const SWIM_SPEED = 100.0
 var direction = 1 
 
+func _ready():
+	$AnimatedSprite2D.play("defaultdiver")
+
 func _process(delta):
 	position.x += SWIM_SPEED * direction * delta
 	$AnimatedSprite2D.flip_h = direction < 0
 	if global_position.x > 2000 or global_position.x < -100:
 		queue_free()
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	queue_free()
+	pass
--- projectile/projectile.gd
+++ projectile/projectile.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-@export var speed = 1000 
+@export var speed = 2000
 var direction = Vector2.ZERO
 
 func _process(delta):
