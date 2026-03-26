528d959703ab569dee2798195278c319895c116d
Bak <bakkdorl@gmail.com>
2026-03-26 18:43:40 -0300

    enemysub two added

---
+++ enemysub_two/enemysub_two.gd
@@ -0,0 +1,20 @@
+extends Area2D
+
+@export var speed = 200.0
+var direction = -1 
+
+func _ready():
+	$AnimatedSprite2D.play("default")
+	$AnimatedSprite2D.flip_h = (direction == 1)
+	add_to_group("enemies")
+
+func _process(delta):
+	position.x += speed * direction * delta
+	
+	if position.x > 2000 or position.x < 1:
+		queue_free()
+
+func _on_area_entered(area):
+	if area.is_in_group("projectiles"):
+		area.queue_free()
+		queue_free()
--- spawn_manager.gd
+++ spawn_manager.gd
@@ -1,11 +1,68 @@
 extends Node2D
 
+#@onready var shark_scene = preload("res://shark.tscn")
+#@onready var enemysub_scene = preload("res://enemysub.tscn")
+#const LANES = [345, 490, 635, 780] 
+#
+#
+#var lane_active = [false, false, false, false]
+#
+#func _ready():
+	#spawn_logic_loop()
+	#
+#func spawn_logic_loop():
+	#var available_indices = []
+	#for i in range(4):
+		#if not lane_active[i]:
+			#available_indices.append(i)
+	#available_indices.shuffle()
+	#var num_to_attempt = randi_range(1, 2)
+	#for i in range(min(num_to_attempt, available_indices.size())):
+		#var idx = available_indices[i]
+		#if not lane_active[idx]:
+			#process_lane_spawn(idx) 
+	#await get_tree().create_timer(randf_range(3.0, 5.0)).timeout
+	#spawn_logic_loop()
+#
+#func process_lane_spawn(lane_idx):
+	#lane_active[lane_idx] = true
+	#var y_pos = LANES[lane_idx]
+	#var dir = 1 if randf() > 0.5 else -1
+	#var spawn_x = 10 if dir == 1 else 1999
+	#var enemy_type = shark_scene if randf() > 0.5 else enemysub_scene
+	#var count = 1
+	#var roll = randf()
+	#if roll < 0.65: 
+		#count = 1
+	#elif roll < 0.90: 
+		#count = 2
+	#else: 
+		#count = 3
+	#for i in range(count):
+		#create_enemy(enemy_type, spawn_x, y_pos, dir)
+		#await get_tree().create_timer(0.8).timeout
+	#await get_tree().create_timer(5.0).timeout
+	#lane_active[lane_idx] = false
+	#
+#func create_enemy(scene, x, y, dir):
+	#var e = scene.instantiate()
+	#e.position = Vector2(x, y)
+	#if "direction" in e:
+		#e.direction = dir
+	#add_child(e)
+	
+	
+	
+	
+#COM ENEMYSUBTWO
 @onready var shark_scene = preload("res://shark.tscn")
 @onready var enemysub_scene = preload("res://enemysub.tscn")
+@onready var enemysub_two_scene = preload("res://enemysub_two.tscn")
+const SURFACE_Y = 250
 const LANES = [345, 490, 635, 780] 
 
-
 var lane_active = [false, false, false, false]
+var surface_active = false 
 
 func _ready():
 	spawn_logic_loop()
@@ -18,9 +75,12 @@ func spawn_logic_loop():
 	available_indices.shuffle()
 	var num_to_attempt = randi_range(1, 2)
 	for i in range(min(num_to_attempt, available_indices.size())):
-		var idx = available_indices[i]
+		var idx = available_indices[i] 
+		process_lane_spawn(idx)
 		if not lane_active[idx]:
 			process_lane_spawn(idx) 
+	if not surface_active and randf() < 0.15: 
+		process_surface_spawn()
 	await get_tree().create_timer(randf_range(3.0, 5.0)).timeout
 	spawn_logic_loop()
 
@@ -51,4 +111,11 @@ func create_enemy(scene, x, y, dir):
 		e.direction = dir
 	add_child(e)
 	
-	
+
+func process_surface_spawn():
+	surface_active = true
+	var dir = 1 if randf() > 0.5 else -1
+	var spawn_x = 10 if dir == 1 else 1999
+	create_enemy(enemysub_two_scene, spawn_x, SURFACE_Y, dir)
+	await get_tree().create_timer(8.0).timeout 
+	surface_active = false
